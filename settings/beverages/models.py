093f29af608adec7a2cba0d636dd837807ac75df
DarthZiel <Studerzhan@gmail.com>
2024-05-17 18:00:45 +0500

    model update

--- settings/beverages/models.py
+++ settings/beverages/models.py
@@ -4,15 +4,15 @@ from django.db import models
 class Coffee(models.Model):
     name = models.CharField(max_length=40, verbose_name='Название кофе')
     description = models.TextField(verbose_name='Описание кофе')
-    image = models.ImageField(verbose_name='Фотография кофе')
-
+    image = models.TextField(verbose_name='Фотография кофе')
+    stars = models.PositiveIntegerField()
     def __str__(self):
         return self.name
 
 class Step(models.Model):
     name = models.CharField(max_length=40, verbose_name='Название шага')
     description = models.TextField(verbose_name='Описание шага')
-    video = models.FileField('видео шага')
+    video = models.TextField('шаг')
     coffee = models.ForeignKey(Coffee, on_delete=models.CASCADE, related_name='steps')
     step_number = models.IntegerField(verbose_name='Номер шага')
 
